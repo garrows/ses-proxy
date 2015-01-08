632dd38d93da6628983534b2f3be8e5315dab66f
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-01-09 08:15:23 +1000

    Ready for publish

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,3 +1,14 @@
 var server = require('../index.js');
 
-server();
+var program = require('commander');
+
+program
+  .version('1.0.0')
+  .option('-p, --port <port>', 'Server port. Defaults to 25')
+  .option('-c, --config <path>', 'set config path. defaults to ./ses-credentials.conf')
+  .parse(process.argv);
+
+program.port = program.port ? program.port : 25;
+program.config = program.config ? program.config : './ses-credentials.json';
+
+server(program.port, program.config);
